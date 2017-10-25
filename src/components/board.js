68f8bae1965818d88079aedd7bc3e4fccfdb7503
f1ux1uxday <fluxpatron@gmail.com>
2017-10-25 00:18:50 -0500

    made AI (so far) unbeatable

--- src/components/board.js
+++ src/components/board.js
@@ -17,28 +17,27 @@ class Board extends Component {
       [3, 6, 9, 12],
     ]
 
-    let CPU_MOVE = () => {
+    let checkForHeat = () => {
       let randNum = Math.floor(Math.random() * 16)
-      console.log(randNum)
 
       if (this.props.board[randNum] !== '') {
-        CPU_MOVE()
+        checkForHeat()
       }
 
       if (this.props.board[randNum] === '' && this.props.turnCounter <= 3) {
+        // For first three turns, mark random cell if checkForDanger fails
         this.props.board[randNum] = this.props.player2symbol
         this.props.mark1P()
-        console.log('turn count ' + this.props.turnCounter)
       } else if (this.props.board[randNum] === '') {
         if (this.props.turnCounter <= 4) {
+          // On fourth turn, find out where the action is
           let matchedArrays = []
-          // Get matching arrays
+          // Get winning arrays that include our random number
           for (let arr in winningArrays) {
             if (winningArrays[arr].includes(randNum)) {
               matchedArrays.push(winningArrays[arr])
             }
           }
-          console.log(matchedArrays)
           // See how many cells in those arrays are marked and by whom
           let count1 = []
           let count2 = []
@@ -52,29 +51,31 @@ class Board extends Component {
               }
             }
           }
+          // Get integers that denote how many symbols each player
+          // has placed into the matched arrays
           let heatCheckerP1 = count1.reduce((sum, val) => {
             return sum + val
           }, 0)
           let heatCheckerP2 = count2.reduce((sum, val) => {
             return sum + val
           }, 0)
-          console.log('hc: ' + heatCheckerP2 + ' / ' + heatCheckerP1)
+          // If one of our matched arrays has been marked,
+          // mark cell at index of randNum. If not, get new randNum.
+          // This prevents pointless moves.
           if (heatCheckerP1 > 0 || heatCheckerP2 > 0) {
             this.props.board[randNum] = this.props.player2symbol
             this.props.mark1P()
-            console.log('turn count ' + this.props.turnCounter)
           } else {
-            CPU_MOVE()
+            checkForHeat()
           }
-        } else if (this.props.turnCounter <= 6) {
+        } else if (this.props.turnCounter >= 5) {
+          // From the fifth term on...
           let matchedArrays = []
           for (let arr in winningArrays) {
             if (winningArrays[arr].includes(randNum)) {
               matchedArrays.push(winningArrays[arr])
             }
           }
-          console.log(matchedArrays)
-          // See how many cells in those arrays are marked and by whom
           let count1 = []
           let count2 = []
           for (let arr in matchedArrays) {
@@ -93,84 +94,59 @@ class Board extends Component {
           let heatCheckerP2 = count2.reduce((sum, val) => {
             return sum + val
           }, 0)
-          console.log('hc : ' + heatCheckerP2 + ' / ' + heatCheckerP1)
+          // Require an array have at least two similar symbols
           if (heatCheckerP1 > 1 || heatCheckerP2 > 1) {
             this.props.board[randNum] = this.props.player2symbol
             this.props.mark1P()
-            console.log('turn count ' + this.props.turnCounter)
           } else {
-            CPU_MOVE()
-          }
-        } else if (this.props.turnCounter > 6) {
-          let matchedArrays = []
-          for (let arr in winningArrays) {
-            if (winningArrays[arr].includes(randNum)) {
-              matchedArrays.push(winningArrays[arr])
-            }
-          }
-          console.log(matchedArrays)
-          // See how many cells in those arrays are marked and by whom
-          let count1 = []
-          let count2 = []
-          for (let arr in matchedArrays) {
-            for (let i in matchedArrays[arr]) {
-              if (this.props.board[i] === this.props.player1symbol) {
-                count1.push(1)
-              }
-              if (this.props.board[i] === this.props.player2symbol) {
-                count2.push(1)
-              }
-            }
-          }
-          let heatCheckerP1 = count1.reduce((sum, val) => {
-            return sum + val
-          }, 0)
-          let heatCheckerP2 = count2.reduce((sum, val) => {
-            return sum + val
-          }, 0)
-          console.log('hc : ' + heatCheckerP2 + ' / ' + heatCheckerP1)
-          if (heatCheckerP1 >= 2 || heatCheckerP2 >= 2) {
-            this.props.board[randNum] = this.props.player2symbol
-            this.props.mark1P()
-            console.log('turn count ' + this.props.turnCounter)
-          } else {
-            CPU_MOVE()
+            checkForHeat()
           }
         }
       }
     }
 
     let checkForDanger = () => {
+      // Initialize placeholder for final index value
       let blockCell = 0
+      // Mark has not yet been marked
       let marked = false
       for (let combo in winningArrays) {
+        // Initialize values for each combo
         let P1symbols = 0
+        let P2symbols = 0
         let emptyCells = 0
         let emptyIndex = 0
+        // And iterate through each combo
         for (let i in winningArrays[combo]) {
           let q = winningArrays[combo][i]
+          // Count the number of P1 and P2 symbols in the combo
           if (this.props.board[q] === this.props.player1symbol) {
             P1symbols++
           }
+          if (this.props.board[q] === this.props.player2symbol) {
+            P2symbols++
+          }
+          // Count the number of empty spaces in the combo
           if (this.props.board[q] === '') {
             emptyCells++
             emptyIndex = q
           }
-          if (P1symbols === 3 && emptyCells === 1) {
+          // If three marked by one player with one empty cell,
+          // mark the empty cell. Danger found!
+          if ((P1symbols === 3 && emptyCells === 1) ||
+              (P2symbols === 3 && emptyCells === 1)) {
             blockCell = emptyIndex
             this.props.board[blockCell] = this.props.player2symbol
             this.props.mark1P()
             marked = true
           }
         }
-        // if (P1symbols === 3 && winningArrays[combo].includes('')) {
-          // blockCell.push(winningArrays[combo].indexOf(''))
-        // }
       }
+      // If danger not found, check for heat
       if (marked === true) {
         return
       } else {
-        CPU_MOVE()
+        checkForHeat()
       }
     }
 
@@ -179,65 +155,53 @@ class Board extends Component {
       this.props.board[0] === this.props.board[1] &&
       this.props.board[0] === this.props.board[2] &&
       this.props.board[0] === this.props.board[3]) {
-        console.log('1')
-        console.log(this.props.active)
         this.props.wingame()
       } else if (this.props.board[4] !== '' &&
       this.props.board[4] === this.props.board[5] &&
       this.props.board[4] === this.props.board[6] &&
       this.props.board[4] === this.props.board[7]) {
-        console.log('2')
         this.props.wingame()
       } else if (this.props.board[8] !== '' &&
       this.props.board[8] === this.props.board[9] &&
       this.props.board[8] === this.props.board[10] &&
       this.props.board[8] === this.props.board[11]) {
-        console.log('3')
         this.props.wingame()
       } else if (this.props.board[12] !== '' &&
       this.props.board[12] === this.props.board[13] &&
       this.props.board[12] === this.props.board[14] &&
       this.props.board[12] === this.props.board[15]) {
-        console.log('4')
         this.props.wingame()
       } else if (this.props.board[0] !== '' &&
       this.props.board[0] === this.props.board[4] &&
       this.props.board[0] === this.props.board[8] &&
       this.props.board[0] === this.props.board[12]) {
-        console.log('5')
         this.props.wingame()
       } else if (this.props.board[1] !== '' &&
       this.props.board[1] === this.props.board[5] &&
       this.props.board[1] === this.props.board[9] &&
       this.props.board[1] === this.props.board[13]) {
-        console.log('6')
         this.props.wingame()
       } else if (this.props.board[2] !== '' &&
       this.props.board[2] === this.props.board[6] &&
       this.props.board[2] === this.props.board[10] &&
       this.props.board[2] === this.props.board[14]) {
-        console.log('7')
         this.props.wingame()
       } else if (this.props.board[3] !== '' &&
       this.props.board[3] === this.props.board[7] &&
       this.props.board[3] === this.props.board[11] &&
       this.props.board[3] === this.props.board[15]) {
-        console.log('8')
         this.props.wingame()
       } else if (this.props.board[0] !== '' &&
       this.props.board[0] === this.props.board[5] &&
       this.props.board[0] === this.props.board[10] &&
       this.props.board[0] === this.props.board[15]) {
-        console.log('9')
         this.props.wingame()
       } else if (this.props.board[3] !== '' &&
       this.props.board[3] === this.props.board[6] &&
       this.props.board[3] === this.props.board[9] &&
       this.props.board[3] === this.props.board[12]) {
-        console.log('10')
         this.props.wingame()
       } else if (this.props.board.indexOf('') === -1) {
-        console.log('draw')
         this.props.tiegame()
       }
     }
@@ -249,12 +213,12 @@ class Board extends Component {
           this.props.board[index] === this.props.player2symbol ||
           this.props.gameOver === 'yes' ||
           this.props.gameOver === 'draw') {
-          // Do nothing if cell is marked or game is over
+          // Empty block: Do nothing if cell is marked or game is over
         } else {
+          // Mark cell and switch active player
           this.props.board[index] = this.props.player1symbol
           this.props.mark1P()
-          console.log('turn count: ' + this.props.turnCounter)
-          // Mark cell and switch active player
+          // Run AI functions and check for end of game
           checkForDanger()
           isItOver()
         }
@@ -267,7 +231,7 @@ class Board extends Component {
         this.props.board[index] === this.props.player2symbol ||
         this.props.gameOver === 'yes' ||
         this.props.gameOver === 'draw') {
-        // Do nothing if cell is marked or game is over
+        // Empty block: Do nothing if cell is marked or game is over
       } else {
         this.props.board[index] = this.props.active
         this.props.mark()
@@ -276,7 +240,6 @@ class Board extends Component {
       isItOver()
       // Victory logic...
     }
-    console.log(index)
   }
 
   render() {
